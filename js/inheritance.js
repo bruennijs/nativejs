59cd643617271c20091a7cae702164c5a43ad1d3
bruenni <oliver.bruentje@gmx.de>
2015-08-16 17:53:00 +0200

    Fixed inheritance js

--- js/inheritance.js
+++ js/inheritance.js
@@ -34,8 +34,7 @@ Child.prototype.getId = function () {
   return this.id;
 }
 
-
-var myapp; = (function () {
+var myapp = (function () {
   return  {
     create: function () {
 
@@ -46,15 +45,13 @@ var myapp; = (function () {
   }
 }());
 
-//var c = new c.Console(process.stdout, process.stderr);
-
-
 var child = new Child("hello world", 5);
 //c.info("create.getName[%s]", myapp.create("hello world").getName());
 c.info("new Person [%s, %s]", child.getName(), child.getId());
 c.info(child.hasOwnProperty('getName'));
 c.info(child.hasOwnProperty('getId'));
 c.info(child.hasOwnProperty('id'));
+c.info(child.hasOwnProperty('name'));
 //c.info("new cp.getName[%s]", new cp("hello world").getName());
 
 
